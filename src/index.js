3cc6476e72562cb10e68471db34560090f1f6c02
andrewgarlick <andrewwgarlick@gmail.com>
2016-09-14 15:08:38 -0600

    work

--- src/index.js
+++ src/index.js
@@ -13,16 +13,27 @@ module.exports = {
 // },
 
   all(settings = { loadAll: false, index: 0, pageSize: 15 }){
-    // check if load all is true
-    return countries;
+    if(settings.loadAll){
+      return countries
+    }
+
 
 
     const startingIndex = settings.index * settings.pageSize;
     // slice the countries array using the paging calculation
 
-    return countries.slice()
+    return countries.slice(startingIndex, startingIndex + settings.pageSize);
   },
-//filter,
+  filter(filterBy){
+    if(!filterBy){
+      return countries;
+    }
+
+    return countries.filter((country) => {
+      return (country.name.common.indexOf(filterBy) > 0) || (country.name.official.indexOf(filterBy) > 0)
+    })
+  }
+
 //get,
 // etc
 
